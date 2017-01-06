436ff2d269f1f73c5312a95a1a64116396f680f0
Gonzalo <gmarquez@mexerp.com>
2017-01-06 14:54:18 -0600

    Initial Set Up using React and Electron

---
+++ process/js/render.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var $ = jQuery = require('jquery');
+var bootstrap = require('bootstrap');
+
+var MainInterface = React.createClass({
+  render: function() {
+    return(
+      <h1>Wisdom Pet Application</h1>
+    )
+  }//render
+}); //MainInterface
+
+ReactDOM.render(
+  <MainInterface />,
+  document.getElementById('petAppointments')
+); //render
